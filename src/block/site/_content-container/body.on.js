ad528718f286963f053a88d448527aa4d2b7ef82
Alexander <alexander.zybin@gmail.com>
2016-04-27 00:12:30 +0300

    201604270012

---
+++ src/block/site/_content-container/body.on.js
@@ -0,0 +1,8 @@
+
+$(document.body).on('click.fecss', '.content-container', {}, function(event, a){
+	
+	$('.menu-container .menu-list .item.dialoged.active').removeClass('active');
+	
+	//var id = $('.content-container')
+	
+});
